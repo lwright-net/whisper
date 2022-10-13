425bb8f8b8f238af1bfa89a0d1a5940f5ff3cf8a
Lane Wright <lane.wright@primecaretech.com>
2022-10-13 17:15:05 +0000

    removed fluff

--- python-emails/imap_select.py
+++
@@ -1,8 +0,0 @@
-import imaplib
-import imaplib_connect
-
-with imaplib_connect.open_connection() as c:
-    typ, data = c.select('INBOX')
-    print(typ, data)
-    num_msg = int(data[0])
-    print('There are {} messages in INBOX'.format(num_msg))
--- python-emails/list_email_folders.py
+++
@@ -1,9 +0,0 @@
-import imaplib
-from pprint import pprint
-from imap_connect import open_connection
-
-with open_connection() as c:
-    typ, data = c.list()
-    print('Response code:', typ)
-    print('Response:')
-    pprint(data)
