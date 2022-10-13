3b3ed714a23c2ad35054a5303849827c5c4bd874
Lane Wright <lane.wright@primecaretech.com>
2022-10-13 16:10:59 +0000

    added send_msg function

--- auto-whisper-test.py
+++ auto-whisper-test.py
@@ -3,15 +3,22 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import whisper
 import mimetypes
-import re
+import smtplib
+from email.message import EmailMessage
+
+whispermodel = 'medium' # tiny, base, small, medium, large
+watcherdir = '.' #directory to be watched
+mailto = 'someone@somehost' #where to send transcripts
+mailfrom = 'autotranscript@somehost' #where did the message come from
+mailserver = 'smtp.somehost' #what smtp server to use
 
 mimetypes.init()
 
-model = whisper.load_model("medium")
+model = whisper.load_model(whispermodel)
 
 class Watcher:
 
-    def __init__(self, directory=".", handler=FileSystemEventHandler()):
+    def __init__(self, directory=watcherdir, handler=FileSystemEventHandler()):
         self.observer = Observer()
         self.handler = handler
         self.directory = directory
@@ -39,11 +46,25 @@ class MyHandler(FileSystemEventHandler):
             newfiletype = mimetypes.guess_type(event.src_path)[0]
             if  newfiletype == "audio/x-wav": #some code to check if audio
                 result = model.transcribe(event.src_path)
-                f = open(event.src_path+".txt", "w+")#some code to write the result to a file
-                f.write(result["text"])
-                f.close()
-                print("wrote " + event.src_path + ".txt")#some code to  print confirmation of transcription
+                #f = open(event.src_path+".txt", "w+")#some code to write the result to a file
+                #f.write(result["text"])
+                #f.close()
+                #print("wrote " + event.src_path + ".txt")#some code to  print confirmation of transcription
+                #mailer.sendmsg('subject with phone number', 'noreply@autotranscript', 'support@ticketsystem', result, event.src_path)
+                mailer.sendmsg('somesubject', mailfrom, mailto, result, event.src_path)
 
+class mailer:
+
+    def send_msg(msgsubject, fromaddr, toaddr, msgbody, msgfile):
+                msg = EmailMessage()
+                msg['Subject'] = msgsubject
+                msg['From'] = fromaddr
+                msg['To'] = toaddr
+                msg['Body'] = msgbody
+                msg.add_attachment(msgfile, maintype='audio', subtype='x-wav')
+
+                with smtplib.SMTP(mailserver) as s:
+                    s.send_message(msg)
 
 if __name__=="__main__":
     w = Watcher(".", MyHandler())
